4ac946ced7e42eccbdd32236df6d81e69377f2a2
Raoelijaona <mitiaraoelijaona1@gmail.com>
2022-09-08 10:30:38 +0300

    add api to get task by id

--- routes/task.js
+++ routes/task.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/taskController');
 const router = express.Router();
 
 router.route('/tasks/:espace').get(controller.getAllTasksBySpace);
-router.route('/tasks/:tache').get(controller.getTaskById);
+router.route('/task/:tache').get(controller.getTaskById);
 router.route('/task/').post(controller.createTasks);
 router.route('/task/:tache').put(controller.updateTask);
 
